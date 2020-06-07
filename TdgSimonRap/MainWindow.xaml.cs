62e8af1e05ab7f02d97fb57fa802956e8c79297d
Tung Nguyen <nguyen.tung@outlook.de>
2020-06-07 20:52:42 +0200

    Projektdateien hinzufügen.

---
+++ TdgSimonRap/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TdgSimonRap
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var names = new List<string> { ":D", "T__T", "ö.ö", ":)", ":(", "D:", ":P", ":Ü", ":O", ";)", ":f", ":3", ":/", "C:", ":I" };
+            var rnd = new Random(); 
+
+            int index = rnd.Next(names.Count);
+            var name = names[index];
+
+            Label1.Content = name;
+
+
+
+        }
+
+    }
+}
